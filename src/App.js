2303655432f61c4b43522ece376aa6f27ff7d590
ceimiplace <100032730+ceimiplace@users.noreply.github.com>
2022-10-03 09:22:25 +0300

    added cards grid

--- src/App.js
+++ src/App.js
@@ -2,22 +2,22 @@ import { useState } from "react";
 import "./App.css";
 const cards = [
   {
-    src: "/image/helmet-1.png",
+    src: "/img/helmet-1.png",
   },
   {
-    src: "/image/potion-1.png",
+    src: "/img/potion-1.png",
   },
   {
-    src: "/image/ring-1.png",
+    src: "/img/ring-1.png",
   },
   {
-    src: "/image/scroll-1.png",
+    src: "/img/scroll-1.png",
   },
   {
-    src: "/image/shield-1.png",
+    src: "/img/shield-1.png",
   },
   {
-    src: "/image/sword-1.png",
+    src: "./img/sword-1.png",
   },
 ];
 function App() {
@@ -36,6 +36,16 @@ function App() {
     <div className="App">
       <h1>Magic Match</h1>
       <button onClick={() => startGame()}>New Game</button>
+      <div className="card-grid">
+        {cardsToDisplay.map((elem) => {
+          return (
+            <div id={elem.id}>
+              <img src={elem.src} alt="card-image" />
+              <img src="/img/cover.png" />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
